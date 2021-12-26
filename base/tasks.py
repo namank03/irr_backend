7e4461b6e06409ee4bccae82786ac1705eee08cb
namank03 <khandelwalnaman03@gmail.com>
2021-12-27 00:03:37 +0530

    added some extra comments

--- base/tasks.py
+++ base/tasks.py
@@ -18,12 +18,14 @@ def celery_function(self, data):
     x5 = df['X5']
     y1 = x3 * x6
     prn_df = pd.concat([(x3 - y1 + x5) * -1, prn_df], axis=1).convert_dtypes()
-
     irr_df = pd.concat([df['X1'], prn_df.apply(npf.irr, axis=1) * 100], axis=1).rename(
         columns={0: "IRR"}
     )
-    res = json.dumps(irr_df.to_dict('records'))
-    for i in range(5):
+    # result = []
+    for i in range(len(df)):
+        # result.append({"X1": df.iloc[i, 0], "IRR": npf.irr(prn_df.iloc[i, :]) * 100})
+        # Simulating the long task behaviour via time.sleep.. If we've a large df we can make the smaller chunks and then we can process that task....
         time.sleep(1)
-        progress_recorder.set_progress(i + 1, 5)
-    return res
+        progress_recorder.set_progress(i + 1, len(df))
+
+    return json.dumps(irr_df.to_dict('records'))
